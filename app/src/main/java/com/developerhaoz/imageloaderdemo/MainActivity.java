a45ec9d71d1b05a1e9768cee57496f1417481643
Haoz <developerhaon@gmail.com>
2017-10-14 17:09:35 +0800

    LayoutInflater 流程基本理解

--- app/src/main/java/com/developerhaoz/imageloaderdemo/MainActivity.java
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.developerhaoz.imageloaderdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
     }
 }
