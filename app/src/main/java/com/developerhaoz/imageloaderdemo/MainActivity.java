6763cf1363165fc5b518c806b70d3c03c178c565
Haoz <developerhaon@gmail.com>
2017-10-15 23:11:12 +0800

    LayoutInflater

--- app/src/main/java/com/developerhaoz/imageloaderdemo/MainActivity.java
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/MainActivity.java
@@ -3,10 +3,13 @@ package com.developerhaoz.imageloaderdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
         LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View view = layoutInflater.inflate(R.layout.activity_main, null);
+        mLinearLayout.getParent();
+
+
     }
 }
