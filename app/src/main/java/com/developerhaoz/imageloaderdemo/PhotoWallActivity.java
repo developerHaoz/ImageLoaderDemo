b91db9e90a45650c6ed125cc7da85c0cfca9cef8
Haoz <developerhaon@gmail.com>
2017-10-23 20:35:31 +0800

    梯子好了，贼开心。

---
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/PhotoWallActivity.java
@@ -0,0 +1,19 @@
+package com.developerhaoz.imageloaderdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Haoz
+ * @date 2017/10/22.
+ */
+
+public class PhotoWallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_wall);
+    }
+}
