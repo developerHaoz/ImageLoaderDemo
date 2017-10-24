0984f7f306664cdd507402aeed962c56997c5b52
Haoz <developerhaon@gmail.com>
2017-10-24 19:12:09 +0800

    开始撸图片加载这一块的。

--- app/src/main/java/com/developerhaoz/imageloaderdemo/PhotoWallActivity.java
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/PhotoWallActivity.java
@@ -3,6 +3,12 @@ package com.developerhaoz.imageloaderdemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * @author Haoz
@@ -11,9 +17,41 @@ import android.support.v7.app.AppCompatActivity;
 
 public class PhotoWallActivity extends AppCompatActivity {
 
+    private ImageView mIv;
+
+    private int mPicsSize;
+
+    private File mImgDir;
+
+    private List<String> mImgs;
+
+    private RecyclerView mRvPhotoWall;
+
+    private HashSet<String> mDirPaths = new HashSet<>();
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_wall);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
