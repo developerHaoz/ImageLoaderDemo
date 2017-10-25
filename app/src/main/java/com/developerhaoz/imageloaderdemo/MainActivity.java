de7d745095fb1d2d7f4d248e29c81ebe1717e6ad
Haoz <developerhaon@gmail.com>
2017-10-25 20:46:47 +0800

    开始研究 Matisse 源码

--- app/src/main/java/com/developerhaoz/imageloaderdemo/MainActivity.java
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/MainActivity.java
@@ -2,8 +2,6 @@ package com.developerhaoz.imageloaderdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        LayoutInflater layoutInflater = LayoutInflater.from(this);
-        View view = layoutInflater.inflate(R.layout.activity_main, null);
     }
 }
