a7b5477b12309ee6b89f1a62bf656c2cadd8efa2
Haoz <developerhaon@gmail.com>
2017-10-31 19:20:24 +0800

    Matisse 主页面源码的阅读，收获很大。

---
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/internal/ui/BasePreviewActivity.java
@@ -0,0 +1,9 @@
+package com.developerhaoz.imageloaderdemo.internal.ui;
+
+/**
+ * @author Haoz
+ * @date 2017/10/31.
+ */
+
+public class BasePreviewActivity {
+}
---
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/internal/ui/adapter/AlbumMediaAdapter.java
@@ -0,0 +1,11 @@
+package com.developerhaoz.imageloaderdemo.internal.ui.adapter;
+
+/**
+ * @author Haoz
+ * @date 2017/10/31.
+ */
+
+public class AlbumMediaAdapter {
+
+
+}
