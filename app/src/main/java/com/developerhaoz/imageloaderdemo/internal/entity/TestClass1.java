687b74d48acbd4df187b2b3b76e4aeabf200bb1f
Haoz <developerhaon@gmail.com>
2017-12-06 21:37:53 +0800

    博客的进展还可以，继续加油。

---
+++ app/src/main/java/com/developerhaoz/imageloaderdemo/internal/entity/TestClass1.java
@@ -0,0 +1,10 @@
+package com.developerhaoz.imageloaderdemo.internal.entity;
+
+/**
+ * @author Haoz
+ * @date 2017/12/6.
+ */
+public class TestClass1 {
+
+    String uri = "content://media/external/file";
+}
